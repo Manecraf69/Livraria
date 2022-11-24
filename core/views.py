a883a1ef09758be8c25d92c4837a526428614646
_manecraf <marcioviniciusvoss@gmail.com>
2022-11-24 18:48:26 -0300

    Usando as permissões do DRF

--- core/views.py
+++ core/views.py
@@ -1,19 +1,31 @@
 from rest_framework.viewsets import ModelViewSet
 
+# from rest_framework.permissions import IsAuthenticated
+
 from core.models import Autor, Categoria, Editora, Livro
-from core.serializers import AutorSerializer, CategoriaSerializer, EditoraSerializer, LivroSerializer
+from core.serializers import (
+    AutorSerializer,
+    CategoriaSerializer,
+    EditoraSerializer,
+    LivroSerializer,
+)
+
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    # permission_classes = [IsAuthenticated]
+
 
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
 
+
 class AutorViewSet(ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
