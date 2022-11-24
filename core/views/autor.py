ae1fb02ffb89bd9bdef583f38957d84fd6e38c65
_manecraf <marcioviniciusvoss@gmail.com>
2022-11-24 20:57:46 -0300

    Reestruturação de models, views e serializers

---
+++ core/views/autor.py
@@ -0,0 +1,14 @@
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Autor, Categoria, Editora, Livro
+from core.serializers import (
+    AutorSerializer,
+    CategoriaSerializer,
+    EditoraSerializer,
+    LivroSerializer,
+)
+
+
+class AutorViewSet(ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
---
+++ core/views/categoria.py
@@ -0,0 +1,14 @@
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Autor, Categoria, Editora, Livro
+from core.serializers import (
+    AutorSerializer,
+    CategoriaSerializer,
+    EditoraSerializer,
+    LivroSerializer,
+)
+
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
